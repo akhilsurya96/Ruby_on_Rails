b911596318c83a682e6581d55fbc8776641490d2
akhilsurya87 <akhil.g@lumiseek.com>
2020-10-04 21:16:21 +0530

    first app

---
+++ listingapp1/app/views/post1s/_post1.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! post1, :id, :title, :genere, :string, :description, :string, :no.of, :pages, :integer, :created_at, :updated_at
+json.url post1_url(post1, format: :json)
---
+++ listingapp1/test/system/post2s_test.rb
@@ -0,0 +1,51 @@
+require "application_system_test_case"
+
+class Post2sTest < ApplicationSystemTestCase
+  setup do
+    @post2 = post2s(:one)
+  end
+
+  test "visiting the index" do
+    visit post2s_url
+    assert_selector "h1", text: "Post2s"
+  end
+
+  test "creating a Post2" do
+    visit post2s_url
+    click_on "New Post2"
+
+    fill_in "Description", with: @post2.description
+    fill_in "Genere", with: @post2.genere
+    fill_in "Npages", with: @post2.npages
+    fill_in "Sno", with: @post2.sno
+    fill_in "Title", with: @post2.title
+    click_on "Create Post2"
+
+    assert_text "Post2 was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Post2" do
+    visit post2s_url
+    click_on "Edit", match: :first
+
+    fill_in "Description", with: @post2.description
+    fill_in "Genere", with: @post2.genere
+    fill_in "Npages", with: @post2.npages
+    fill_in "Sno", with: @post2.sno
+    fill_in "Title", with: @post2.title
+    click_on "Update Post2"
+
+    assert_text "Post2 was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Post2" do
+    visit post2s_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Post2 was successfully destroyed"
+  end
+end
